d819d4ef375a1005fdc22d864fe09d67d6cc9e5a
Karol Nowakowski <przeslijmi@gmail.com>
2019-09-10 17:52:59 +0200

    v1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v1.1.0 - 2019-09-10
+
+- Added: `configSpecimen.php` to resources.
+
 ## v1.0.0 - 2019-08-09
 
 - Added `CHANGELOG.md`.
---
+++ resources/configSpecimen.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+use Przeslijmi\Sirouter\Sirouter;
+
+
+
+/////////////////////////
+// Routes for testing. //
+/////////////////////////
+
+require('vendor/przeslijmi/sirouter/tests/Helpers/ExampleRsrc.php');
+
+Sirouter::register('/_test_', 'GET')
+    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'get');
+
+Sirouter::register('/_test_/(\d+)/params/(\d+)', 'PUT')
+    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'put')
+    ->setParam(0, 'testParam1')
+    ->setParam(1, 'testParam2');
+
+Sirouter::register('/_testScalar_', 'GET')
+    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'getScalar');
+
+Sirouter::register('/_testWrotype_', 'GET')
+    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'getWrotype');
+
+// method 'delete' is not present in the class on test purpose
+Sirouter::register('/_test_', 'DELETE')
+    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'delete');
+
+// this class do not exists on test purpose
+Sirouter::register('/_testNonexistingClass_', 'GET')
+    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleNonexistingRsrc', 'get');
+
+// this class exists but is not Rsrc class on test purpose
+Sirouter::register('/_testNonRsrcClass_', 'GET')
+    ->setCall('Przeslijmi\Sirouter\Sirouter', 'get');

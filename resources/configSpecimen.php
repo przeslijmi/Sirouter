7aeb16c7819c4b3003c0ae1613c4f92e994d94f0
Karol Nowakowski <przeslijmi@gmail.com>
2020-04-14 13:50:00 +0200

    shippable

--- resources/configSpecimen.php
+++ resources/configSpecimen.php
@@ -1,35 +1,33 @@
 <?php declare(strict_types=1);
 
 use Przeslijmi\Sirouter\Sirouter;
-
+use Przeslijmi\Sirouter\Helpers\ExampleRsrc;
 
 
 /////////////////////////
 // Routes for testing. //
 /////////////////////////
 
-require('vendor/przeslijmi/sirouter/tests/Helpers/ExampleRsrc.php');
-
 Sirouter::register('/_test_', 'GET')
-    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'get');
+    ->setCall(ExampleRsrc::class, 'get');
 
 Sirouter::register('/_test_/(\d+)/params/(\d+)', 'PUT')
-    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'put')
+    ->setCall(ExampleRsrc::class, 'put')
     ->setParam(0, 'testParam1')
     ->setParam(1, 'testParam2');
 
 Sirouter::register('/_testScalar_', 'GET')
-    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'getScalar');
+    ->setCall(ExampleRsrc::class, 'getScalar');
 
 Sirouter::register('/_testWrotype_', 'GET')
-    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'getWrotype');
+    ->setCall(ExampleRsrc::class, 'getWrotype');
 
 Sirouter::register('/_testTxt_', 'GET')
-    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'getTxt');
+    ->setCall(ExampleRsrc::class, 'getTxt');
 
 // method 'delete' is not present in the class on test purpose
 Sirouter::register('/_test_', 'DELETE')
-    ->setCall('Przeslijmi\Sirouter\Helpers\ExampleRsrc', 'delete');
+    ->setCall(ExampleRsrc::class, 'delete');
 
 // this class do not exists on test purpose
 Sirouter::register('/_testNonexistingClass_', 'GET')
